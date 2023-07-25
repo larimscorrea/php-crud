5d759ff714056d973abe582f14ed4e8ad539ce31
CPDrogas <coordepesquisacpdrogas@gmail.com>
2023-07-25 12:06:11 -0300

    página de consulta

--- consulta.php
+++ consulta.php
@@ -1,3 +1,23 @@
+<h1>Formulário de cadastro</h1>
+<form action="receber.php" method="POST">
+    
+    <input type="text" name="search">
+
+
+
+    Selecione o bairro
+    <select name="ficha" onchange="exibirFicha(this)">
+      <option value="" data-info="" data-info-primaria=""></option>
+      <option value="ficha">ficha</option>
+      <option value="semana 1">Semana 1</option>
+      <option value="semana 2">Semana 2</option>
+    </select>
+
+    <p> <a href="#"> nome </a> | <a href="#"> cpf </a>  | <a href="#"> nº da ficha </a>  </p>
+
+    <input type="submit" value="Pesquisar" />
+</form>
+
 <?php 
 $host = 'localhost';
 $usuario = 'root';
