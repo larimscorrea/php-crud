93ca5e460cbbdd1db5cd2ecdc52ef0a3939f4d9e
CPDrogas <coordepesquisacpdrogas@gmail.com>
2023-07-26 15:16:23 -0300

    função de pesquisar funcionando, agora só preciso resolver o problema da impressão

--- consulta.php
+++ consulta.php
@@ -1,34 +1,63 @@
-<h1>Formulário de cadastro</h1>
-<form action="receber.php" method="POST">
-    
+<h1>Formulário de consulta</h1>
+<form action="consulta.php" method="POST">
     <input type="text" name="search">
-
-
-
-    Selecione o bairro
+    Selecione a ficha
     <select name="ficha" onchange="exibirFicha(this)">
-      <option value="" data-info="" data-info-primaria=""></option>
-      <option value="ficha">ficha</option>
-      <option value="semana 1">Semana 1</option>
-      <option value="semana 2">Semana 2</option>
+        <option value="" data-info="" data-info-primaria=""></option>
+        <option value="ficha">ficha</option>
+        <option value="semana 1">Semana 1</option>
+        <option value="semana 2">Semana 2</option>
     </select>
-
-    <p> <a href="#"> nome </a> | <a href="#"> cpf </a>  | <a href="#"> nº da ficha </a>  </p>
-
     <input type="submit" value="Pesquisar" />
 </form>
 
 <?php 
-$host = 'localhost';
-$usuario = 'root';
-$senha = '';
-$data = 'teste-project';
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Verificar se o campo de pesquisa foi preenchido
+    if (isset($_POST["search"]) && !empty($_POST["search"])) {
+        // Obter o valor do campo de pesquisa
+        $searchValue = $_POST["search"];
 
-$conn = mysqli_connect($host,$usuario,$senha,$data);
+        // Obter o valor selecionado do campo ficha
+        $fichaValue = $_POST["ficha"];
 
-$sql = "SELECT * from cadastro";
+        // Criar a consulta SQL com base nos critérios de pesquisa
+        $sql = "SELECT * FROM cadastro WHERE nome LIKE '%$searchValue%'";
 
-mysqli_query($conn,$sql);
+        // // Se uma ficha específica foi selecionada, adicionar filtro à consulta
+        if (!empty($fichaValue)) {
+            $sql .= " AND ficha = '$fichaValue'";
+        }
+      
+        // Conectar-se ao banco de dados
+        $host = 'localhost';
+        $usuario = 'root';
+        $senha = '';
+        $data = 'teste-project';
+        $conn = mysqli_connect($host, $usuario, $senha, $data);
 
+        // Verificar se a conexão foi bem-sucedida
+        if ($conn) {
+            // Executar a consulta
+            $result = mysqli_query($conn, $sql);
 
+            // Verificar se a consulta retornou resultados
+            if (mysqli_num_rows($result) > 0) {
+                // Exibir os resultados da consulta
+                while ($row = mysqli_fetch_assoc($result)) {
+                    echo "<p>{$row['nome']} | {$row['cpf']} | {$row['ficha']}</p>";
+                }
+            } else {
+                echo "<p>Nenhum resultado encontrado.</p>";
+            }
+
+            // Fechar a conexão
+            mysqli_close($conn);
+        } else {
+            echo "Falha na conexão: " . mysqli_connect_error();
+        }
+    } else {
+        echo "Por favor, insira um valor para pesquisar.";
+    }
+}
 ?>
--- receber.php
+++ receber.php
@@ -1,47 +1,40 @@
 <?php
-if (isset($_POST['nomeusuario']) && isset($_POST['senha']) && isset($_POST['bairro'])) {
-    $host = 'localhost';
-    $usuario = 'root';
-    $senha = '';
-    $data = 'teste-project';
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (isset($_POST['nomeusuario']) && isset($_POST['senha']) && isset($_POST['bairro']) && isset($_POST['genero'])) {
+        $host = 'localhost';
+        $usuario = 'root';
+        $senha = '';
+        $data = 'teste-project';
 
-    $conn = mysqli_connect($host, $usuario, $senha, $data);
+        $conn = mysqli_connect($host, $usuario, $senha, $data);
 
-    if ($conn) {
-        echo "Conexão estabelecida com sucesso.";
+        if ($conn) {
+            echo "Conexão estabelecida com sucesso.";
 
-        $nome = $_POST['nomeusuario'];
-        $senha = $_POST['senha'];
-        $bairro = $_POST['bairro'];
-        $genero = $_POST['genero'];
+            $nome = $_POST['nomeusuario'];
+            $senha = $_POST['senha'];
+            $bairro = $_POST['bairro'];
+            $genero = $_POST['genero'];
 
-       
-        // Separar os valores enviados usando explode
-        list($bairroValue, $regionalValue, $territorioValue) = explode('|', $bairro);
+            // Separar os valores enviados usando explode
+            list($bairroValue, $regionalValue, $territorioValue) = explode('|', $bairro);
 
+            $sql = "INSERT INTO cadastro (nome, bairro, regional, territorio, genero, senha) VALUES ('$nome', '$bairroValue', '$regionalValue', '$territorioValue', '$genero', '$senha')";
 
-        $sql = "INSERT INTO cadastro (nome, bairro, regional, territorio, genero, senha) VALUES ('$nome', '$bairroValue', '$regionalValue', '$territorioValue', '$genero', '$senha')";
+            if (mysqli_query($conn, $sql)) {
+                echo "Registro inserido com sucesso.";
+            } else {
+                echo "Erro ao inserir registro: " . mysqli_error($conn);
+            }
 
-        if (mysqli_query($conn, $sql)) {
-            echo " Registro inserido com sucesso.";
+            mysqli_close($conn);
         } else {
-            echo " Erro ao inserir registro: " . mysqli_error($conn);
+            echo "Falha na conexão: " . mysqli_connect_error();
         }
-
-        mysqli_close($conn);
     } else {
-        echo " Falha na conexão: " . mysqli_connect_error();
+        echo "Campos do formulário não estão definidos.";
     }
 } else {
-    echo " Campos de formulário não estão definidos.";
+    echo "Requisição inválida.";
 }
 ?>
-
-
-
-
-
-
-
-
-
