7489e1099572c2274159dddb778ce1042295673e
CPDrogas <coordepesquisacpdrogas@gmail.com>
2023-08-02 09:45:10 -0300

    tentativa

--- cadastrar.php
+++ cadastrar.php
@@ -43,3 +43,41 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     echo "Requisição inválida.";
 }
 ?>
+
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (isset($_POST['nome']) && isset($_POST['senha'])) {
+        $host = 'localhost';
+        $usuario = 'root';
+        $senha = '';
+        $data = 'teste-project';
+
+        $conn = mysqli_connect($host, $usuario, $senha, $data);
+
+        if ($conn) {
+            echo "Conexão estabelecida com sucesso.";
+
+            $nome = $_POST['nome'];
+            $senha = $_POST['senha'];
+            
+            $sql = "INSERT INTO usuarios (nome, senha) VALUES ('$nome', '$senha')";
+
+            if (mysqli_query($conn, $sql)) {
+                echo "Registro inserido com sucesso.";
+            } else {
+                echo "Erro ao inserir registro: " . mysqli_error($conn);
+            }
+
+            mysqli_close($conn);
+        } else {
+            echo "Falha na conexão: " . mysqli_connect_error();
+        }
+    } else {
+        echo "Campos do formulário não estão definidos.";
+    }  
+
+} else {
+    echo "Requisição inválida.";
+}
+
+?>
